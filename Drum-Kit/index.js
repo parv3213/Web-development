e49552f5ae1b1efd0a99ac5e35762fe01957b414
parv3213 <parv3213@gmail.com>
2020-04-20 01:01:18 +0530

    completed

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -33,6 +33,35 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     });
 }
 
+document.addEventListener("keypress", function(e){
+    switch (e.key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3")
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3")
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3")
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3")
+            break;
+        case "j":
+            var audio = new Audio("sounds/kick-bass.mp3")
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3")
+            break;
+        case "l":
+            var audio = new Audio("sounds/snare.mp3")
+            break;
+
+        default:
+            break;
+    }
+    audio.play();
+});
 
 // var audio = new Audio("sounds/tom-1.mp3");
 // audio.play();
