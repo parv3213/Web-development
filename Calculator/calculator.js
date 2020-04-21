e8fcdf2f4b4990f2713ffc57b1384500e9f26cec
parv3213 <parv3213@gmail.com>
2020-04-21 17:41:19 +0530

    calculator

---
+++ Calculator/calculator.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The Answer is:" + result); 
+});
+
+app.listen(3000, function () {
+    console.log("Local server running at port 3000");
+});
