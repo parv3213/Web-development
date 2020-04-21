8fa9060fb0037dd3fbed3c85613c340965a4e70b
parv3213 <parv3213@gmail.com>
2020-04-21 18:12:11 +0530

    cmi commit

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -8,11 +8,25 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmiCalculator", function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
 app.post("/", function (req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
-    res.send("The Answer is:" + result); 
+    res.send("The Answer is: " + result); 
+});
+
+app.post("/bmiCalculator", (req,res) => {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height*height);
+    console.log(weight);
+    console.log(height);
+    console.log(bmi);
+    res.send("Your BMI is:  "+bmi);
 });
 
 app.listen(3000, function () {
