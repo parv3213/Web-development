e803ebbaa78b8635dd2c5b6f9cc061a24742c7b2
parv3213 <parv3213@gmail.com>
2020-04-22 05:55:17 +0530

    initialise the newsletter-app

---
+++ Newsletter-signup/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running");
+});
