4d0efa2f03c5f52b9e4360516757175207fe77d0
parv3213 <parv3213@gmail.com>
2020-04-25 11:10:44 +0530

    beautify

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # web-development-bootcamp
-Following  Dr. Angela Yu.
+
+Following Dr. Angela Yu.
 
 1. HTML_PERSONAL_SITES: https://parv3213.github.io/Web-development/HTML-Personal-Sites/index.html
 2. CSS-My-Site: https://parv3213.github.io/Web-development/CSS-My-Site/index.html
 3. Tindog Website: https://parv3213.github.io/Web-development/TinDog/index.html
 4. Dicee Chalange: https://parv3213.github.io/Web-development/Dicee-Challenge/dicee.html
 5. Drum-Kit https://parv3213.github.io/Web-development/Drum-Kit/index.html
-5. Simon-Game https://parv3213.github.io/Web-development/Simon-Game/Simon-Game.html
-6. Newsletter-App https://newsletter-app-parv.herokuapp.com/ (temp-not-working)
-7. Todo-List-1 https://todo-list-parv.herokuapp.com/
-8. Daily Journal(ejs-challange) https://daily-journal-parv.herokuapp.com/
+6. Simon-Game https://parv3213.github.io/Web-development/Simon-Game/Simon-Game.html
+7. Newsletter-App https://newsletter-app-parv.herokuapp.com/ (temp-not-working)
+8. TodoList-V1 https://todo-list-parv.herokuapp.com/
+9. Daily Journal(ejs-challange) https://daily-journal-parv.herokuapp.com/
--- TodoList-V1/app.js
+++ TodoList-V1/app.js
@@ -1,11 +1,10 @@
 const express = require("./node_modules/express");
 const bodyParser = require("./node_modules/body-parser");
-const date = require(__dirname+"/date.js")
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,36 +12,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const todos = ["Buy Food", "Cook Food", "Eat Food"];
 var workItems = [];
 
-app.get("/", (req, res) => {    
-    res.render("list", { listTitle: date.getDate(), todos: todos });
+app.get("/", (req, res) => {
+  res.render("list", { listTitle: date.getDate(), todos: todos });
 });
 
 app.post("/", (req, res) => {
-    const newTodo = req.body.newTodo;
+  const newTodo = req.body.newTodo;
 
-    if (req.body.list === "Work List") {
-        workItems.push(newTodo);
-        res.redirect("/work");
-    } else {
-        todos.push(newTodo);
-        res.redirect("/");
-    }
-
+  if (req.body.list === "Work List") {
+    workItems.push(newTodo);
+    res.redirect("/work");
+  } else {
+    todos.push(newTodo);
+    res.redirect("/");
+  }
 });
 
 app.get("/work", (req, res) => {
-    res.render("list", { listTitle: "Work List", todos: workItems });
+  res.render("list", { listTitle: "Work List", todos: workItems });
 });
 
 app.post("/work", (req, res) => {
-    res.redirect("/work");
+  res.redirect("/work");
 });
 
-app.get("/about" ,(req,res) => {
-    res.render("about");
+app.get("/about", (req, res) => {
+  res.render("about");
 });
-
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
