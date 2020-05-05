7823461e3358fd2c68bb346a827ab7f198001bb6
parv3213 <parv3213@gmail.com>
2020-05-05 10:03:54 +0530

    keeper-app-part-2 completed

--- React/keeper-app-part-2/src/components/App.js
+++ React/keeper-app-part-2/src/components/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Note />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			{notes.map((note) => {
+				return <Note key={note.key} title={note.title} content={note.content} />;
+			})}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- React/keeper-app-part-2/src/components/Note.js
+++ React/keeper-app-part-2/src/components/Note.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Note() {
-  return (
-    <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
-    </div>
-  );
+function Note(props) {
+	return (
+		<div className="note">
+			<h1>{props.title}</h1>
+			<p>{props.content}</p>
+		</div>
+	);
 }
 
 export default Note;
