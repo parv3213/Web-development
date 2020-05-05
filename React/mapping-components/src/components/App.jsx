f37b77cd0854962719969d7bc5bd7002bacb7b3c
parv3213 <parv3213@gmail.com>
2020-05-05 09:17:24 +0530

    mapping-components-practice completed

--- React/mapping-components/src/components/App.jsx
+++ React/mapping-components/src/components/App.jsx
@@ -3,24 +3,24 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function createCard(contact) {
-    return (
-        <Card
-            key={contact.id}
-            name={contact.name}
-            imgURL={contact.imgURL}
-            phone={contact.phone}
-            email={contact.email}
-        />
-    );
+	return (
+		<Card
+			key={contact.id}
+			name={contact.name}
+			imgURL={contact.imgURL}
+			phone={contact.phone}
+			email={contact.email}
+		/>
+	);
 }
 
 function App() {
-    return (
-        <div>
-            <h1 className="heading">My Contacts</h1>
-            {contacts.map(createCard)}
-        </div>
-    );
+	return (
+		<div>
+			<h1 className="heading">My Contacts</h1>
+			{contacts.map(createCard)}
+		</div>
+	);
 }
 
 export default App;
