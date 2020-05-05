3147919848581a8a2760ca2eba20556cbc7e3893
parv3213 <parv3213@gmail.com>
2020-05-05 21:28:01 +0530

    usestate-hook-practice starting files

---
+++ React/usestate-hook-practice/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>TIME</h1>
+      <button>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
