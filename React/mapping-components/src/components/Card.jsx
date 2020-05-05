c68553987689bb83dac2c8e4187b931e4dff3592
parv3213 <parv3213@gmail.com>
2020-05-05 08:19:09 +0530

    react-mapping-components starting file

---
+++ React/mapping-components/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ React/mapping-components/src/components/Detail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Detail(props) {
+  return <p className="info">{props.detailInfo}</p>;
+}
+
+export default Detail;
