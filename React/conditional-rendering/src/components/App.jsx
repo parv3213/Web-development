4e6d2a872ed1b92cb41791c8fda0777efdb704f2
parv3213 <parv3213@gmail.com>
2020-05-05 14:39:03 +0530

    conditional-rendering complete

--- React/conditional-rendering/src/components/App.jsx
+++ React/conditional-rendering/src/components/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	return <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>;
 }
 
 export default App;
