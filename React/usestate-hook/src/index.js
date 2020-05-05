823e70095700f393867d43455f03f2011359f2b9
parv3213 <parv3213@gmail.com>
2020-05-05 14:54:58 +0530

    usestate-hook starting files

---
+++ React/usestate-hook/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div className="container">
+    <h1>0</h1>
+    <button>+</button>
+  </div>,
+  document.getElementById("root")
+);
