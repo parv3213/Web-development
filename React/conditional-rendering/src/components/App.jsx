5214715aa2a842343730d907e8434d9793a4ffb1
parv3213 <parv3213@gmail.com>
2020-05-05 10:06:02 +0530

    conditional-rendering initial files

---
+++ React/conditional-rendering/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
