dc8ad6188c9615b2cb96ed55ffd8f3b15a7fbaf4
parv3213 <parv3213@gmail.com>
2020-05-05 09:31:07 +0530

    map-filter-reduce starting files

---
+++ React/map-filter-reduce/src/index.js
@@ -0,0 +1,11 @@
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
