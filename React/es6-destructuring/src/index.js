1eda7c4b5773dcb159ecb369ed0ed661257e6182
parv3213 <parv3213@gmail.com>
2020-05-06 03:59:53 +0530

    es6-destructuring completed and new files

--- React/es6-destructuring/src/index.js
+++ React/es6-destructuring/src/index.js
@@ -1,23 +1,38 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = cars;
+const {
+	speedStats: { TopColour: hondaTopSpeed },
+} = honda;
+const {
+	speedStats: { TopColour: teslaTopSpeed },
+} = tesla;
+const {
+	coloursByPopularity: [teslaTopColour],
+} = tesla;
+const {
+	coloursByPopularity: [hondaTopColour],
+} = honda;
+
+ReactDOM.render(
+	<table>
+		<tr>
+			<th>Brand</th>
+			<th>Top Speed</th>
+		</tr>
+		<tr>
+			<td>{tesla.model}</td>
+			<td>{teslaTopSpeed}</td>
+			<td>{teslaTopColour}</td>
+		</tr>
+		<tr>
+			<td>{honda.model}</td>
+			<td>{hondaTopSpeed}</td>
+			<td>{hondaTopColour}</td>
+		</tr>
+	</table>,
+	document.getElementById("root")
+);
