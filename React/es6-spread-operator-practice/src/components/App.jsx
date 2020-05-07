b13edd99e0517536e5f45a513556ffd30bbfaaca
parv3213 <parv3213@gmail.com>
2020-05-08 00:43:20 +0530

    es6-spread-operator-practice completed

--- React/es6-spread-operator-practice/src/components/App.jsx
+++ React/es6-spread-operator-practice/src/components/App.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
-      </div>
-    </div>
-  );
+	const [item, setItem] = React.useState("");
+	const [todoList, setTodoList] = React.useState([]);
+
+	function handleChange(event) {
+		setItem(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		setTodoList((prevValue) => [...prevValue, item]);
+		setItem("");
+		event.preventDefault();
+	}
+
+	return (
+		<div className="container">
+			<div className="heading">
+				<h1>To-Do List</h1>
+			</div>
+			<div className="form">
+				<form onSubmit={handleSubmit}>
+					<input onChange={handleChange} type="text" value={item} />
+					<button type="submit">
+						<span>Add</span>
+					</button>
+				</form>
+			</div>
+			<div>
+				<ul>
+					{todoList.map((todo, index) => (
+						<li key={index}>{todo}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
