0a17dec158686c02ea97c219fadb27a6a84fa65d
parv3213 <parv3213@gmail.com>
2020-05-07 10:17:59 +0530

    changing-complex-state completed

--- React/changing-complex-state/src/components/App.jsx
+++ React/changing-complex-state/src/components/App.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+	const [fullName, setFullName] = useState({
+		fName: "",
+		lName: "",
+	});
+
+	function handleChange(event) {
+		const { value, name } = event.target;
+
+		setFullName((prevValue) => {
+			if (name === "fName") {
+				return { fName: value, lName: prevValue.lName };
+			} else {
+				return { fName: prevValue.fName, lName: value };
+			}
+		});
+	}
+
+	return (
+		<div className="container">
+			<h1>
+				Hello {fullName.fName} {fullName.lName}
+			</h1>
+			<form>
+				<input onChange={handleChange} value={fullName.fName} name="fName" placeholder="First Name" />
+				<input onChange={handleChange} value={fullName.lName} name="lName" placeholder="Last Name" />
+				<button>Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
