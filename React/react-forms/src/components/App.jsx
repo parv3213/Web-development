643c153efabbcaefcc00489939cd9cef3a1e9778
parv3213 <parv3213@gmail.com>
2020-05-07 09:03:09 +0530

    react-forms completed

--- React/react-forms/src/components/App.jsx
+++ React/react-forms/src/components/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+	const [name, setName] = React.useState("");
+	const [publishName, setPublishName] = React.useState("");
+
+	function handleChange(event) {
+		setName(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		setPublishName(name);
+		event.preventDefault();
+	}
+
+	return (
+		<div className="container">
+			<h1>Hello {publishName}</h1>
+			<form onSubmit={handleSubmit}>
+				<input onChange={handleChange} value={name} type="text" placeholder="What's your name?" />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
