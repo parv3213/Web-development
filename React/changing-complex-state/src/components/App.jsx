f7a887597ee355ea419b721559b55cfd23b2f811
parv3213 <parv3213@gmail.com>
2020-05-07 09:10:06 +0530

    changing-complex-state starting files

---
+++ React/changing-complex-state/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
