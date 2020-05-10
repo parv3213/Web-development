be40f4c1db4ae9651483765a50ef7520b51b7ee9
parv3213 <parv3213@gmail.com>
2020-05-10 09:18:16 +0530

    using-pre-built-react-components completed

--- React/using-pre-built-react-components/src/components/CreateArea.jsx
+++ React/using-pre-built-react-components/src/components/CreateArea.jsx
@@ -1,51 +1,62 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
-  const [note, setNote] = useState({
-    title: "",
-    content: ""
-  });
+	const [note, setNote] = useState({
+		title: "",
+		content: "",
+	});
+	const [zoom, setZoom] = React.useState(false);
 
-  function handleChange(event) {
-    const { name, value } = event.target;
+	function handleChange(event) {
+		const { name, value } = event.target;
 
-    setNote(prevNote => {
-      return {
-        ...prevNote,
-        [name]: value
-      };
-    });
-  }
+		setNote((prevNote) => {
+			return {
+				...prevNote,
+				[name]: value,
+			};
+		});
+	}
 
-  function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    });
-    event.preventDefault();
-  }
+	function submitNote(event) {
+		props.onAdd(note);
+		setNote({
+			title: "",
+			content: "",
+		});
+		event.preventDefault();
+	}
 
-  return (
-    <div>
-      <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <button onClick={submitNote}>Add</button>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form className="create-note">
+				<input
+					name="title"
+					style={zoom === false ? { display: "none" } : { display: "inherit" }}
+					onChange={handleChange}
+					value={note.title}
+					placeholder="Title"
+					autoComplete="off"
+				/>
+				<textarea
+					name="content"
+					onClick={() => setZoom(true)}
+					onChange={handleChange}
+					value={note.content}
+					placeholder="Take a note..."
+					rows={zoom === false ? 1 : 3}
+				/>
+				<Zoom in={zoom}>
+					<Fab onClick={submitNote}>
+						<AddIcon />
+					</Fab>
+				</Zoom>
+			</form>
+		</div>
+	);
 }
 
 export default CreateArea;
