3d977e9d4b934de0c32e021920ed6a784cb3abaa
parv3213 <parv3213@gmail.com>
2020-07-23 22:27:12 +0530

    added react build

---
+++ React/using-pre-built-react-components/build/precache-manifest.cfbc72f77b74b32112506c007450f0a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b032b281fd5494505aaf5a77a3be39fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eaa39e634b95e2d2b03",
+    "url": "/static/js/2.0f60d888.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.0f60d888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2b759ad9e024b583eb",
+    "url": "/static/js/main.3625d365.chunk.js"
+  },
+  {
+    "revision": "272b2118ce1a3384d9bd",
+    "url": "/static/js/runtime-main.342c7561.js"
+  }
+]);
